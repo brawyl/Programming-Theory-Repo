43b0b4a92d4cec1d22e079f22431501d661dd8e9
brawyl <bwylie@ualberta.ca>
2021-06-16 11:35:43 -0600

    Adds comments for OOP pillars

--- Programming Theory Project/Assets/Scripts/BlueRacer.cs
+++ Programming Theory Project/Assets/Scripts/BlueRacer.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
+// INHERITANCE
 public class BlueRacer : Racer
 {
+    // ENCAPSULATION
     public override string racerName { get { return "blue"; } }
     public override string description { get { return "This racer is a blue ball. \n\nspeed: " + this.speed + "\nluck: " + this.luck; } }
     public override float speed { get { return 20f; } }
     public override float luck { get { return 50f; } }
+
+    // POLYMORPHISM
+    public override void LuckyBoost()
+    {
+        float randomNumber = Random.Range(0f, 100f);
+        if (randomNumber <= luck)
+        {
+            Accelerate();
+        }
+    }
 }
--- Programming Theory Project/Assets/Scripts/GreenRacer.cs
+++ Programming Theory Project/Assets/Scripts/GreenRacer.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 
+// INHERITANCE
 public class GreenRacer : Racer
 {
+    // ENCAPSULATION
     public override string racerName { get { return "green"; } }
     public override string description { get { return "This racer is a green ball. \n\nspeed: " + this.speed + "\nluck: " + this.luck; } }
     public override float speed { get { return 22f; } }
